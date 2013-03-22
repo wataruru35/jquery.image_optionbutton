9237e22e111ebf3109159892cd42131921d28e5a
watanabemasaaki <watanabe@1star.jp>
2013-03-22 11:44:13 +0900

    first commit

---
+++ image_optionbutton/js/jquery.image_optionbutton.js
@@ -0,0 +1,33 @@
+(function( $ ) {
+
+	$.fn.optionButton = function() {
+	
+		var all = $(this);
+		
+		//selectedをカラーにする
+		$("[selected]").css({"-webkit-filter":"grayscale(0%)","filter":"none"});
+		
+		all.click(function () {
+			
+			all.removeAttr("style");
+			
+			$(this).css({"-webkit-filter":"grayscale(0%)","filter":"none"});
+			
+			$("[selected]").removeAttr("selected");
+			
+			$(this).attr("selected","selected");
+			
+		});
+		
+	};
+	
+	
+	$.fn.selectedValue = function() {
+	
+		var val = $("[selected]").attr("val")
+	
+		return val;
+		
+	};
+
+})( jQuery );
